94e67199d1ccbb131601ae2fa480b7c923f8fe9d
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-05-16 21:54:02 -0700

    toggle item display and filtering menu

--- app.js
+++ app.js
@@ -54,6 +54,15 @@ const app = new Vue({
     el: '#media-list',
     data: {
       title: 'Treehouse Public Library',
-      mediaList: media
+      mediaList: media,
+      type: ''
+    },
+    methods: {
+      toggleDetails: function(media){
+        media.showDetail = !media.showDetail;
+      },
+      filterList: function(){
+        this.type = event.target.value;
+      }
     }
-  });
+  }); 
